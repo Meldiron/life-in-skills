68a731f4255f90c4feed18cac6261cbae61a5b23
Matej Bačo <matejbaco2000@gmail.com>
2024-08-17 12:54:06 +0200

    Fix auth page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,14 +7,14 @@
 	<div class="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
 		<a
 			class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-white text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none"
-			href="https://life-in-skills.authui.site/"
+			href="https://life-in-skills.almostapps.eu/"
 		>
 			Create account
 		</a>
 
 		<a
 			class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-neutral-800 text-neutral-400 hover:bg-neutral-700 hover:text-neutral-100 focus:text-neutral-100 focus:outline-none focus:bg-neutral-700 disabled:opacity-50 disabled:pointer-events-none"
-			href="https://life-in-skills.authui.site/"
+			href="https://life-in-skills.almostapps.eu/"
 		>
 			Sign in
 		</a>
@@ -23,7 +23,7 @@
 	<div class="mt-4">
 		<a
 			class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-neutral-800 text-neutral-400 hover:bg-neutral-700 hover:text-neutral-100 focus:text-neutral-100 focus:outline-none focus:bg-neutral-700 disabled:opacity-50 disabled:pointer-events-none"
-			href="https://life-in-skills.authui.site/"
+			href="https://life-in-skills.almostapps.eu/"
 		>
 			Play as guest
 		</a>
