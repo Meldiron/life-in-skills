a7146699ea3a08494c04b9baa4a2643af415ba9b
Matej Bačo <matejbaco2000@gmail.com>
2024-08-18 00:29:27 +0200

    Curses UI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,14 +14,6 @@
 		{
 			url: '/app/curses',
 			name: 'Curses'
-		},
-		{
-			url: '/app/shop',
-			name: 'Shop'
-		},
-		{
-			url: '/app/hero',
-			name: 'Hero'
 		}
 	];
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/preline/preline.js'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Poppins', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 	plugins: [require('preline/plugin')]
 };
