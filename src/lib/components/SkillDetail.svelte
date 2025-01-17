1e4f2d662a522d609f8d529da58b20d3a843b8e4
Matej Bačo <matejbaco2000@gmail.com>
2025-01-17 13:19:17 +0100

    Design fix

--- src/lib/components/SkillDetail.svelte
+++ src/lib/components/SkillDetail.svelte
@@ -44,7 +44,7 @@
 	role="dialog"
 	tabindex="-1"
 >
-	<div class="flex justify-between items-center py-2 px-4">
+	<div class="flex sm:flex-row gap-4 sm:gap-2 flex-col-reverse justify-between items-center py-2 px-4">
 		<div class="flex gap-2 items-center gap-3">
 			<div class="bg-neutral-800 text-neutral-400 text-center min-w-[45px] py-2 px-3 text-2xl rounded-xl h-[fit-content]">
 				<span>{skill.icon ?? ''}</span>
@@ -64,7 +64,7 @@
 			</div>
 		</div>
 
-		<div class="items-center gap-2">
+		<div class="flex items-center gap-3 sm:gap-2 flex-shrink-0">
 			{#if admin}
 				<button
 					onclick={editSkill}
